9e7752cbc45d50dcd83ade3ca4d910ef460d0aa4
Harikarthick <karthickaitech@gmail.com>
2018-11-28 14:08:38 +0530

    Initial Commit

---
+++ HandleError/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  HandleError
+//
+//  Created by Anilkumar on 27/11/18.
+//  Copyright © 2018 Anilkumar. All rights reserved.
+//
+
+import UIKit
+
+enum StudentError: Error {
+    case invalid(String)
+    case tooShort
+}
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        do{
+            var s = try Student(name: nil)
+        }
+        catch let error
+        {
+            print(error)
+        }
+
+        
+
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+ 
+    
+
+
+}
+class Student {
+    var name: String?
+    init(name: String?) throws {
+        guard let name = name else{
+            throw StudentError.invalid("Invalid")
+        }
+        self.name = name }
+    
+    func myName(str: String) throws -> String {
+        
+        guard str.count > 5
+            else{
+                throw StudentError.tooShort
+        }
+        
+        return "My name is \(str)"
+    }
+}
+
